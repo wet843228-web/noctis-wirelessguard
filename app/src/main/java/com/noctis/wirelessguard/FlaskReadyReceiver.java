f1512b4a1e2f956aced023cd6c2b14a033fdefea
wet843228-web <you@example.com>
2026-07-20 05:52:47 +0900

    Add FlaskReadyReceiver: staged startup via custom broadcast instead of BOOT_COMPLETED race

---
+++ app/src/main/java/com/noctis/wirelessguard/FlaskReadyReceiver.java
@@ -0,0 +1,24 @@
+package com.noctis.wirelessguard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class FlaskReadyReceiver extends BroadcastReceiver {
+
+    public static final String ACTION_FLASK_READY =
+            "com.noctis.wirelessguard.FLASK_READY";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (ACTION_FLASK_READY.equals(intent.getAction())) {
+            Intent svc = new Intent(context, DefenderOverlayService.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(svc);
+            } else {
+                context.startService(svc);
+            }
+        }
+    }
+}
